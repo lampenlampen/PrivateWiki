72985ecd489f0c0015080c933f0edc82f751da3a
Felix Zacher <felix.zacher@outlook.de>
2019-10-14 15:39:03 +0200

    Added Extension function to Color

--- PrivateWiki/Utilities/ColorExtension.cs
+++ PrivateWiki/Utilities/ColorExtension.cs
@@ -1,12 +1,18 @@
 ﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
 
 namespace PrivateWiki.Utilities
 {
 	static class ColorExtension
 	{
-		public static Windows.UI.Color ToWindowsUiColor(this System.Drawing.Color old)
+		public static Windows.UI.Color ToWindowsUiColor(this System.Drawing.Color color)
 		{
-			return Color.FromArgb(old.A, old.R, old.G, old.B);
+			return Color.FromArgb(color.A, color.R, color.G, color.B);
+		}
+
+		public static SolidColorBrush ToBrush(this System.Drawing.Color color)
+		{
+			return new SolidColorBrush(color.ToWindowsUiColor());
 		}
 	}
 }
