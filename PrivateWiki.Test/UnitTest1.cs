0b14889c6c2324c969b9e3584b6947953d17a0f4
Felix Zacher <felix.zacher@outlook.de>
2020-06-17 22:03:23 +0200

    Add XUnit Test Project

---
+++ PrivateWiki.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PrivateWiki.Test
+{
+	public class UnitTest1
+	{
+		private readonly ITestOutputHelper output;
+
+		public UnitTest1(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		[Fact]
+		public void Test1()
+		{
+		}
+	}
+}
