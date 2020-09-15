c7ec6135970c9381d5282a75a3b4ab18d92895b6
Felix Zacher <felix.zacher@outlook.de>
2020-09-15 23:58:10 +0200

    ui tweaks

--- PrivateWiki.UWP/UI/Pages/SettingsPages/LabelsSettingsPage.xaml.cs
+++ PrivateWiki.UWP/UI/Pages/SettingsPages/LabelsSettingsPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using PrivateWiki.ViewModels.Settings;
 using ReactiveUI;
 
@@ -42,17 +30,14 @@ namespace PrivateWiki.UWP.UI.Pages.SettingsPages
 		}
 
 		#endregion
-		
+
 		public LabelsSettingsPage()
 		{
 			this.InitializeComponent();
-			
+
 			ViewModel = new LabelsSettingsPageViewModel();
 
-			this.WhenActivated(disposable =>
-			{
-
-			});
+			this.WhenActivated(disposable => { });
 		}
 	}
 }
